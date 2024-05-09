835dacd9d6be77d82a2ef693bcbe0b0ebc35b3ea
Coletinn <gustavocoleto@hotmail.com>
2024-05-09 11:30:42 -0300

    Migrated from GitLab to GitHub

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+import secrets
+
+from flask import Flask
+from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+
+from db import db
+import models
+
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)    
+
+    api = Api(app)
+    
+    app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128)
+    jwt = JWTManager()
+
+    @app.before_request
+    def create_tables():
+        db.create_all()
+
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
+
+    return app  # Return the app instance
+
+
+if __name__ == "__main__":
+    app = create_app()  # Create the app using the factory function
+    app.run(debug=True)
---
+++ resources/user.py
@@ -0,0 +1,55 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+
+from db import db
+from models import UserModel
+from schemas import UserSchema
+
+
+blp = Blueprint("Users", "users", description="Operations on users")
+
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, data):
+        if UserModel.query.filter(UserModel.username == data["username"]).first():
+            abort(409, message="A user with that username already exists!")
+
+        user = UserModel(
+            username=data["username"],
+            password=pbkdf2_sha256.hash(data["password"])
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        return "User created successfully", 201
+
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, data):
+        user = UserModel.query.filter(
+            username = data["username"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(data["password"], user.password):
+            
+
+
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return "User deleted", 201
